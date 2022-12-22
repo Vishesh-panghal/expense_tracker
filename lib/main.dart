3088953eb932609ec6fb4f4defb7a500e5a39891
Vishesh Panghal <visheshpanghal12@gmail.com>
2022-12-22 09:24:24 +0530

    separate widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/widget/button.dart';
+import '../widget/user_transaction.dart';
 
+// !To run APP..📲
 void main() => runApp(MyApp());
 
+// TODO Statefull Widget or Stateless Widget
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,29 +16,54 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+// transitions from transition.dart file
+  final titleController = TextEditingController();
+
+  final amtcontroller = TextEditingController();
+
+  void startNewTransaction(BuildContext ctx){
+    showModalBottomSheet(context: ctx, builder: (_){return NewTransaction(addTx)})
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // just like frame for a screen
       appBar: AppBar(
         title: const Text('Expense Tracker'),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () => {},
+              icon: Icon(
+                Icons.add,
+                color: Colors.white,
+              ))
+        ],
       ),
       body: Column(
-        mainAxisAlignment:
-            MainAxisAlignment.start, // it is from Top to Bottom Horizontally
+        // mainAxisAlignment: MainAxisAlignment
+        //     .spaceAround, // it is from Top to Bottom Horizontally
         crossAxisAlignment:
-            CrossAxisAlignment.center, // it is from Left to Right Vertically
+            CrossAxisAlignment.stretch, // it is from Left to Right Vertically
         children: <Widget>[
           Container(
             width: double.infinity,
             child: Card(color: Colors.blue, elevation: 10, child: Text('Card')),
           ),
-          Card(
-            color: Colors.amber,
-            child: Text('List of Transitions..💵'),
-          )
+          UserTransaction()
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: (() => {}),
+          child: Icon(
+            Icons.add,
+          )),
     );
   }
 }
